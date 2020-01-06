c14bd3a5bce79d22cbd68afeb160d18c12b86817
wyunchi1990 <wang935863431@126.com>
2020-01-07 00:02:32 +0800

    add readme

--- game-api/src/main/java/org/linlinjava/litemall/wx/web/DebugController.java
+++ game-api/src/main/java/org/linlinjava/litemall/wx/web/DebugController.java
@@ -1,10 +1,11 @@
 package org.linlinjava.litemall.wx.web;
 
+import org.linlinjava.litemall.gameserver.game.GameObjectChar;
+import org.linlinjava.litemall.gameserver.game.GameObjectCharMng;
 import org.linlinjava.litemall.gameserver.service.TitleService;
 import org.linlinjava.litemall.wx.request.GrantTitleRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,13 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class DebugController {
     private final Logger logger = LoggerFactory.getLogger(DebugController.class);
 
-    @Autowired
-    private TitleService titleService;
-
     @PostMapping("/grant-title")
     public Object grantTitle(@RequestBody GrantTitleRequest request) {
         logger.error("grantTitle: " + request.toString());
-        titleService.grantTitle(request.getUid(), request.getSource(), request.getTitle());
+
+        GameObjectChar gameObjectChar = GameObjectCharMng.getGameObjectChar(request.getUid());
+        TitleService.grantTitle(gameObjectChar, request.getSource(), request.getTitle());
         return true;
     }
 }
